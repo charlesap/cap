9584b9f1bd1739b3c9bcbb50d75a77bf700fe48c
Chuck <chuck@memetech.com>
2010-12-24 14:48:00 -0800

    adding a utility script extracted from cap.i

---
+++ cap.xtrfile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash                                                                                                                       
+# This is cap.xtrfile.sh                                                                                                          
+
+  echo "Extracting File" $1 " as " $2 from cap.i
+  awk -v pats="$1 ~ " -v pat1e=":xcpt~ \"$1" -v pat2e=":xcps~ \"$1" '                                                                                          
+   BEGIN { pout = 0 }                                                                                                             
+   $0 ~ pats { pout = 1 }                                                                                                         
+   $0 ~ pat1e { pout = 0 }                                                                                                         
+   $0 ~ pat2e { pout = 0 }                                                                                                         
+   pout > 1 {print}                                                                                                               
+   pout > 0 { pout++ }                                                                                                            
+  ' cap.i > ./$2
